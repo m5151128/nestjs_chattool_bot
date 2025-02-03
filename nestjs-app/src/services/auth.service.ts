d6fbaa2a7c50a672e4e669a585b8d036812b54bd
m5151128 <nagayama@thedott.io>
2025-02-03 16:52:34 +0900

    トークン生成処理

--- nestjs-app/src/services/auth.service.ts
+++ nestjs-app/src/services/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  createToken(userId: string, email: string) {
+    // TODO: RS256で行うように変更
+    const header = {
+      alg: 'HS256',
+      kid: 'collabo_tool_kid',
+    };
+    const payload = {
+      sub: userId,
+      email: email,
+    };
+    const secretKey = 'collabo_tool_secret_key';
+
+    return jwt.sign(payload, secretKey, {
+      algorithm: "HS256",
+      expiresIn: '1h',
+      header: header,
+    });
+  }
+}
