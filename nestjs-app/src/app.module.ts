879a29074e1050ec4d9064a4c88e5473800d5733
m5151128 <nagayama@thedott.io>
2025-02-03 13:40:19 +0900

    nest generate service services/slack --flat

--- nestjs-app/src/app.module.ts
+++ nestjs-app/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BtsController } from './controllers/bts.controller';
+import { SlackService } from './services/slack.service';
 
 @Module({
   imports: [],
   controllers: [AppController, BtsController],
-  providers: [AppService],
+  providers: [AppService, SlackService],
 })
 export class AppModule {}
